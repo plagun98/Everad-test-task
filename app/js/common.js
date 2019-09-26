2ac5c554318005296a473fc4f4be84d5eebd9632
Alexandr <plagun98@gmail.com>
2019-09-27 00:25:54 +0300

    full responsive till 320px

--- app/js/common.js
+++ app/js/common.js
@@ -2,11 +2,19 @@ window.onload = function(){
     if($(window).width() <= 1200){
         $(".mikocin-info__inner-wrapper__right").appendTo($(".for-mobile"));
     }
+    if($(window).width() <= 600){
+        $(".mobile-header p")[0].innerHTML = "Доставка бесплатно";
+    }
 }
 window.onresize = function(){
     if($(window).width() <= 1200){
         $(".mikocin-info__inner-wrapper__right").appendTo($(".for-mobile"));
     } else {
         $(".mikocin-info__inner-wrapper__right").appendTo($(".mikocin-info__inner-wrapper"));
+    }
+    if($(window).width() <= 600){
+        $(".mobile-header p")[0].innerHTML = "Доставка бесплатно";
+    } else {
+        $(".mobile-header p")[0].innerHTML = "Бесплатно";
     }
 }
